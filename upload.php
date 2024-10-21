5838cde8c924f591d8c1c9e3764bdf5e59598590
Rahil Poladiya <76041675+truemaverick33@users.noreply.github.com>
2024-10-21 23:11:23 +0530

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,36 +1,34 @@
 <?php
-header('Access-Control-Allow-Origin:http://localhost:3000');
+header('Access-Control-Allow-Origin:http://localhost:3000'); 
+$title = $_POST['bname'];
 
+$toDay = date("jnyHis");
 
+$file_name = $_FILES['fupload']['name'];
+$file_size = $_FILES['fupload']['size'];
+$file_tmp = $_FILES['fupload']['tmp_name'];
+$file_type = $_FILES['fupload']['type'];
+move_uploaded_file($file_tmp,"books/".$toDay.str_replace(' ', '',$file_name));
+$file_loc = "books/".$toDay.str_replace(' ', '',$file_name);
 
+$file_name = $_FILES['cover']['name'];
+$file_size = $_FILES['cover']['size'];
+$file_tmp = $_FILES['cover']['tmp_name'];
+$file_type = $_FILES['cover']['type'];
+move_uploaded_file($file_tmp,"covers/".$toDay.str_replace(' ', '',$file_name));
+$cover_loc = "covers/".$toDay.str_replace(' ', '',$file_name);
 
 $host = "localhost";
 $username = "root";
 $password = "";
 $database = "book_db.sql";
 $conn=mysqli_connect($host, $username, $password, $database);
-// $datafor=$_POST['datafor'];
-
-// Directory where files will be uploaded
-// $targetDir = ;
-// $targetC = ;
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-$file_name = $_FILES['fupload']['name'];
-$file_size = $_FILES['fupload']['size'];
-$file_tmp = $_FILES['fupload']['tmp_name'];
-$file_type = $_FILES['fupload']['type'];
-move_uploaded_file($file_tmp,"C:/Program Files/Wamp/www/backend/books/".$file_name);
-echo "Book uploaded successfully";
-
-$cover_name = $_FILES['cover']['name'];
-$cover_size = $_FILES['cover']['size'];
-$cover_tmp = $_FILES['cover']['tmp_name'];
-$cover_type = $_FILES['cover']['type'];
-
-move_uploaded_file($cover_tmp,"C:/Program Files/Wamp/www/backend/covers/".$cover_name);
-echo "Cover uploaded successfully";
+$sql = "INSERT INTO book_master(title,loc,cover) VALUES('$title','$file_loc','$cover_loc')";
+if(mysqli_query($conn,$sql))
+	{
+		header('location:http://localhost:3000/');
+	}
+	else{
+		echo "Error";
+	}
 ?>
